c84a631844da6c9575d126b5e9b88f2b53d68a1c
BAMOUH <e.bamouh@enim.ac.ma>
2020-12-03 01:14:52 +0100

    click on recycleView and show items details was added

--- android/app/src/main/java/com/ismin/projectapp/StationViewHolder.kt
+++ android/app/src/main/java/com/ismin/projectapp/StationViewHolder.kt
@@ -1,11 +1,24 @@
 package com.ismin.projectapp
 
+import android.content.Intent
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class StationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class StationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
     var txvName = itemView.findViewById<TextView>(R.id.r_station_txv_name)
     var txvEbike: TextView = itemView.findViewById(R.id.r_station_txv_ebike)
     var txvMechanical: TextView = itemView.findViewById(R.id.r_station_txv_mechanical)
+
+
+    fun initialize(item: Station, action: OnStationListener){
+
+            itemView.setOnClickListener { View ->
+                action.onStationClick(item, adapterPosition)
+            }
+    }
+
+
 }
