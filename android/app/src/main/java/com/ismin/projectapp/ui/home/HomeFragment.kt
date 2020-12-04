acf1c9a5a9cdd4e95c661b451d21948ce7790e12
BAMOUH <e.bamouh@enim.ac.ma>
2020-12-04 22:28:13 +0100

    fixing problem

--- android/app/src/main/java/com/ismin/projectapp/ui/home/HomeFragment.kt
+++ android/app/src/main/java/com/ismin/projectapp/ui/home/HomeFragment.kt
@@ -123,8 +123,7 @@ class HomeFragment : Fragment() {
         //for database
         dbHandler = DatabaseHandler(root.context, null, null, 1)
         //dbHandler.deleteStation("42027") //test remove
-        //var dbStations: ArrayList<Station> = getStationsFromDb(root)
-        var dbStations: ArrayList<Station> = dbHandler.getFavoriteOnDB(root.context)
+        var dbStations: ArrayList<Station> = getStationsFromDb(root)
 
         val stationListFragment = StationListFragment.newInstance(dbStations)
 
